758bd677802506b8de4395be8a5f393bced89098
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-01 21:48:02 +0300

    exercise 6 uploaded

---
+++ exercise 6/main.js
@@ -0,0 +1,8 @@
+let colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+
