89eeafd2039b13d4a1402364e91b43ae5eb03138
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-01 22:54:44 +0300

    exercise 9

---
+++ exercise 9/main.js
@@ -0,0 +1,21 @@
+let libarary =[
+{
+        title:"salmoy salaamaad",
+        author:"Mohamed Haji",
+        year:2020
+    },
+{
+        title:"aqrista wanaagsan",
+        author:"yusuf gedi",
+        year:2026
+    },
+{
+        title:"suugaan",
+        author:"Abdiaziz Mohamed",
+        year:2013
+    },
+]
+
+
+console.log(libarary[0].title);
+console.log(libarary[1].author);
