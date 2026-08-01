43683a59711a2697a03d0bacfda5f375a0d89e75
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-01 22:39:26 +0300

    exercise  uplaoded

--- exercise 8/main.js
+++ exercise 8/main.js
@@ -1,9 +1,17 @@
 let car ={
     make: "Toyota",
      model: "Camry",
-      year: 2020
+      year: 2020,
+       start : function () {
+        console.log("The car has started");
+       }
     };
 
     console.log(car.make);
     console.log(car.model);
     console.log(car.year);
+
+
+
+   
+   car.start();
