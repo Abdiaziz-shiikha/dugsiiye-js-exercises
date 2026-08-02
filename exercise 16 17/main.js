22765736a6564bed7c1ea531c5d061c94f238901
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-02 12:34:00 +0300

    upload the exercise 17

--- exercise 16 17/main.js
+++ exercise 16 17/main.js
@@ -1,6 +1,23 @@
-let score = 90;
-if (score >= 50){
-    console.log("You have passed!");
+
+//kan wa exercise 16
+
+// let score = 90;
+// if (score >= 50){
+//     console.log("You have passed!");
+// } else {
+//     console.log("You have failed.");
+// }
+
+//kan wa exercise 17
+
+let temperature = 8;
+
+if (temperature <= 0) {
+    console.log("It's very cold!");
+} else if (temperature <= 15) {
+    console.log("It's cool.");
+} else if( temperature <= 25) {
+    console.log("It's warm.");
 } else {
-    console.log("You have failed.");
+    console.log("It's hot.");
 }
