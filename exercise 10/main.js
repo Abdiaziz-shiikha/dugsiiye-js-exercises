45c1e7ec778889ed0c68342f18b07c0fd3e569d3
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-02 10:36:17 +0300

    upload the exercise 12

--- exercise 10/main.js
+++ exercise 10/main.js
@@ -14,3 +14,13 @@ let fruits = ["banana", "apple", "cherry"];
 for(let i=0 ; i<fruits.length ; i++){
     console.log(fruits[i]);
 }
+
+
+
+//kan wa exercise 12
+
+let i=5;
+while(i>=0){
+    console.log(i);
+    i--;
+}
