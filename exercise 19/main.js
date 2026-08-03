22828cc86d89dc4e871486369f3cffe26ef5056c
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-03 12:32:32 +0300

    exeercise 22 uploaded

--- exercise 19/main.js
+++ exercise 19/main.js
@@ -2,28 +2,24 @@
 //kan wa exercise 19
 
 
-// const numbers = [1, 2, 3, 4, 5];
 
-// numbers.forEach((number) => {
-//     console.log(number);
-// });
 
 //kan wa exercise 20
 
-// const fruits = ["apple", "banana", "cherry"];
+const fruits = ["apple", "banana", "cherry"];
 
-// const lengths = fruits.map((fruit)  => {
-//    return fruit.length;
-// });
+const lengths = fruits.map((fruit)  => {
+   return fruit.length;
+});
 
-// console.log(lengths); 
+console.log(lengths); 
 
 //kan wa exercise 21
 
-// const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5];
 
-// const sum =numbers.reduce((total,tiro) => total + tiro, 0);
-// console.log(sum);
+const sum =numbers.reduce((total,tiro) => total + tiro, 0);
+console.log(sum);
 
 //kan wa exercise 22
 
@@ -32,3 +28,17 @@ const colors = ["red", "green", "blue"];
 const [item1, item2, item3] = colors;
 console.log(item1);
 console.log(item3);
+
+const cars={
+    make: "Toyota",
+    model: "Camry",
+    year: 2020
+};
+
+
+
+
+let{make,model,year}=cars;
+
+console.log(make);
+console.log(year);
