a4a79fa44ececa59d6692b90276d50e0d623cbaa
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-03 14:47:07 +0300

    uploaded exercise 27

--- exercise 26/main.js
+++ exercise 26/main.js
@@ -23,3 +23,25 @@ dataOfUser (function user(user){
 
 
 console.log("mudane wax yar dulqaado!")
+
+
+
+// kan waa exercise 27
+
+function fetchDatyOfUser(){
+    return new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        
+        const success=true;
+        if (success){
+            resolve("is real user")
+        }else{
+            reject("failed fetching")
+        }
+       
+    }, 2000);
+})}
+
+fetchDatyOfUser()
+.then(data=>(console.log(data)))
+.catch(err=>(console.log(err)))
