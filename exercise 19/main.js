8dcab394e50ca0b383f008c40bcab7f2aeecfc27
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-03 12:11:34 +0300

    exercise 21  uploaded

--- exercise 19/main.js
+++ exercise 19/main.js
@@ -2,11 +2,11 @@
 //kan wa exercise 19
 
 
-// const numbers = [1, 2, 3, 4, 5];
+const numbers = [1, 2, 3, 4, 5];
 
-// numbers.forEach((number) => {
-//     console.log(number);
-// });
+numbers.forEach((number) => {
+    console.log(number);
+});
 
 //kan wa exercise 20
 
@@ -16,4 +16,12 @@ const lengths = fruits.map((fruit)  => {
    return fruit.length;
 });
 
-console.log(lengths); 
+// console.log(lengths); 
+
+//kan wa exercise 21
+
+const numbers = [1, 2, 3, 4, 5];
+
+const sum =numbers.reduce((total,tiro) => total + tiro, 0);
+console.log(sum);
+
