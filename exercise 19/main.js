b37ecfd724bf21567ba9faeb8107ea836c68f431
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-03 12:23:23 +0300

    exercise 22 uploaded

--- exercise 19/main.js
+++ exercise 19/main.js
@@ -2,26 +2,33 @@
 //kan wa exercise 19
 
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach((number) => {
-    console.log(number);
-});
+// numbers.forEach((number) => {
+//     console.log(number);
+// });
 
 //kan wa exercise 20
 
-const fruits = ["apple", "banana", "cherry"];
+// const fruits = ["apple", "banana", "cherry"];
 
-const lengths = fruits.map((fruit)  => {
-   return fruit.length;
-});
+// const lengths = fruits.map((fruit)  => {
+//    return fruit.length;
+// });
 
 // console.log(lengths); 
 
 //kan wa exercise 21
 
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-const sum =numbers.reduce((total,tiro) => total + tiro, 0);
-console.log(sum);
+// const sum =numbers.reduce((total,tiro) => total + tiro, 0);
+// console.log(sum);
 
+//kan wa exercise 22
+
+const colors = ["red", "green", "blue"];
+
+const [item1, item2, item3] = colors;
+console.log(item1);
+console.log(item3);
