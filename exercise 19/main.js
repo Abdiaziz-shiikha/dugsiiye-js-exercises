6168a46bcd4b339624c28f55e15c063e721f1379
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-03 13:44:49 +0300

    upload exercise 25

--- exercise 19/main.js
+++ exercise 19/main.js
@@ -1,5 +1,5 @@
 
-// //kan wa exercise 19
+ //kan wa exercise 19
 
 const numbers = [1, 2, 3, 4, 5];
 
@@ -8,7 +8,7 @@ numbers.forEach((number) => {
 });
 
 
-// //kan wa exercise 20
+ //kan wa exercise 20
 
 const fruits = ["apple", "banana", "cherry"];
 
@@ -18,7 +18,7 @@ const lengths = fruits.map((fruit)  => {
 
 console.log(lengths); 
 
-// //kan wa exercise 21
+ //kan wa exercise 21
 
 const numbers = [1, 2, 3, 4, 5];
 
@@ -32,6 +32,8 @@ const colors = ["red", "green", "blue"];
 const [item1, item2, item3] = colors;
 console.log(item1);
 console.log(item3);
+
+//kan waa exercise 23
 
 const cars={
     make: "Toyota",
@@ -47,10 +49,21 @@ let{make,model,year}=cars;
 console.log(make);
 console.log(year);
 
-//kan waa exercise 23
+//kan waa exercise 24
 
 function calculateArea(width,height=width){
     console.log(`the area is ${width*height}M`);
 }
 
-calculateArea(10,10);
+// kan waa exercise 25
+
+
+const num1=[1,2,4];
+const allNumbures=[...num1,4,5,6];
+console.log(allNumbures);
+
+function mul(...tiro){
+   return tiro.reduce((result,num) => result * num ,1);
+}
+console.log(mul(2,3,5));
+
