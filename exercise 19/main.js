8c205dfc78c61f2f5234e5a31f66c50325679fab
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-03 11:49:08 +0300

    exercise 20  uploaded

--- exercise 19/main.js
+++ exercise 19/main.js
@@ -1,5 +1,19 @@
-const numbers = [1, 2, 3, 4, 5];
 
-numbers.forEach((number) => {
-    console.log(number);
+//kan wa exercise 19
+
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// numbers.forEach((number) => {
+//     console.log(number);
+// });
+
+//kan wa exercise 20
+
+const fruits = ["apple", "banana", "cherry"];
+
+const lengths = fruits.map((fruit)  => {
+   return fruit.length;
 });
+
+console.log(lengths); 
