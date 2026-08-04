782d00a20098e6388e53972135c90748dc7076cd
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-04 14:52:53 +0300

    31 uploaded

--- exercise 26/main.js
+++ exercise 26/main.js
@@ -91,3 +91,37 @@ async function datyFetch() {
 }
 
 datyFetch();
+
+
+
+//kan waa exercise 30
+
+function operate(a, b, callback) {
+    return callback(a, b);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+
+
+console.log("Addition:", operate(5, 3, add));       
+console.log("Subtraction:", operate(5, 3, subtract));
+console.log("multiplay: " , operate(9,6 ,multiply));
+console.log("division: " , operate(9,6 ,divide));
+
+
+
