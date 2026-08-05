1e9d7066f026cf192a9f3259a1ee095420f0fbea
Abdiaziz-shiikha <abdiazizgoobe@gmail.com>
2026-08-05 14:01:14 +0300

    exercise 33 uploaded

--- exercise 32/main.js
+++ exercise 32/main.js
@@ -1,5 +1,17 @@
+//kan waa exercise 32
+
 const hero = document.querySelector('#hero');
 console.log(hero);
 
-const information = document.querySelectorAll('.info');
+const information = document.querySelector('#info');
 console.log(information);
+
+function changeContent(){
+
+    hero.textContent = "welcome to world wide ";
+
+}
+
+function repeat(){
+    information.innerHTML = "this the best web uses dom manipulation <strong>java script<strong>"
+}
